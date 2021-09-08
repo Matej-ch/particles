78f66b6ec1ce33845c24e57610bcdaaf695b16cb
Matej <matejchalachan@gmail.com>
2021-09-08 21:23:20 +0200

    update

--- src/js/Particle.js
+++ src/js/Particle.js
@@ -43,17 +43,17 @@ class Particle {
     if (distance < mouse.radius + this.size) {
       this.color = 'DarkOrange';
       if (mouse.x < this.x && this.x < canvas.width - this.size * 10) {
-        this.x += 10;
+        this.x += 5;
 
       }
       if (mouse.x > this.x && this.x > this.size * 10) {
-        this.x -= 10;
+        this.x -= 5;
       }
       if (mouse.y < this.y && this.y < canvas.height - this.size * 10) {
-        this.y += 10;
+        this.y += 5;
       }
       if (mouse.y > this.y && this.y > this.size * 10) {
-        this.y -= 10;
+        this.y -= 5;
       }
     } else {
       this.color = 'gold';
